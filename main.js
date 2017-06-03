47c0d87b8c668b62be080a038356c30dc1c893bd
Kasey Z <qicong.zheng@gmail.com>
2017-06-04 03:44:43 +0800

    jQuery finish upper panel

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var session=parseInt(sLow.text());
   var breaks=parseInt(bLow.text());
   var arr=$("#clock").text().split(':')
-  var hour=parseInt(arr[0]);
+  $hour=parseInt(arr[0]);
   var minute=parseInt(arr[1]);
   var clock=$("#clock");
 
@@ -12,12 +12,15 @@ $(document).ready(function(){
   $(".plus.session").on("click",function(){
     session=session+1
     sLow.text(session);
-
+    $hour=session;
+    clockFace($hour, minute);
   });
 
   $(".minor.session").on("click",function(){
      session=session-1;
      sLow.text(session);
+     $hour=session;
+     clockFace(hour, minute);
   });
 
   $(".plus.break").on("click",function(){
@@ -44,4 +47,16 @@ $(document).ready(function(){
       top:'0'},100);
   });
 
+   $('.stop').on("click",function(){
+     clockFace($hour, 0);
+   });
+
+
 });
+
+function clockFace (hour, minute){
+   if (minute<10){ minute='0' + minute}
+   else {minute=minute }
+   time=hour + ':' + minute;
+   $("#clock").text(time);
+};

4c0bac03242dc7b3d2ee54eecc950df984e339e5
Kasey Z <qicong.zheng@gmail.com>
2017-06-04 22:53:32 +0800

    finished all the function

---
+++ README.md
@@ -0,0 +1,3 @@
+The Pomodoro Technique is a time management system where you work for a period of time (Session) then take a short break, then work for a period of time then take a short break, and so on.
+
+This is the solution for the Odin Project, course Web Development 101 / Pair Programming.
--- main.js
+++ main.js
@@ -1,36 +1,49 @@
 $(document).ready(function(){
   var sLow=$("p.session.low");
   var bLow=$("p.break.low");
-  var session=parseInt(sLow.text());
-  var breaks=parseInt(bLow.text());
-  var arr=$("#clock").text().split(':')
-  $hour=parseInt(arr[0]);
-  var minute=parseInt(arr[1]);
+  $session=parseInt(sLow.text());
+  $breaks=parseInt(bLow.text());
+  $arr=$("#clock").text().split(':');
+  $minute=parseInt($arr[0]);
+  $second=parseInt($arr[1]);
   var clock=$("#clock");
+  $tickTuck=false;
+  $totalTime=($minute *60)+$second;
 
 
   $(".plus.session").on("click",function(){
-    session=session+1
-    sLow.text(session);
-    $hour=session;
-    clockFace($hour, minute);
+    if ($tickTuck==false) {
+        $session++;
+        sLow.text($session);
+        $minute=$session;
+        clockFace($minute, 0);
+    }
   });
 
   $(".minor.session").on("click",function(){
-     session=session-1;
-     sLow.text(session);
-     $hour=session;
-     clockFace(hour, minute);
+    if ($tickTuck==false) {
+      if ($session > 1){
+           $session--;
+           sLow.text($session);
+           $minute=$session;
+           clockFace($minute, 0);}
+   }
   });
 
   $(".plus.break").on("click",function(){
-    breaks=breaks+1;
-    bLow.text(breaks);
+    if ($tickTuck==false) {
+    $breaks++;
+    bLow.text($breaks);
+    }
   });
 
   $(".minor.break").on("click",function(){
-     breaks=breaks-1;
-     bLow.text(breaks);
+    if ($tickTuck==false) {
+      if ($breaks >1){
+        $breaks--;
+        bLow.text($breaks);
+      }
+     }
   });
 
   $(".manu").mousedown(function(){
@@ -47,16 +60,86 @@ $(document).ready(function(){
       top:'0'},100);
   });
 
-   $('.stop').on("click",function(){
-     clockFace($hour, 0);
-   });
+  $('.reset').on('click',function(){
+    clockFace(25,0);
+    sLow.text(25);
+    bLow.text(5);
+    $('#clock').css('color','white');
+    $('#big').text('Session');
+  });
 
+  $('.start').on('click',function(){
+    $tickTuck=true;
+    $arr=$("#clock").text().split(':');
+    $minute=parseInt($arr[0]);
+    $second=parseInt($arr[1]);
+    $intervalId=setInterval(startClock, 1000);
+
+
+
+
+    $('.pause').on('click',function(){
+      clearInterval($intervalId);
+    });
+
+    $('.stop').on("click",function(){
+      clearInterval($intervalId);
+      clockFace($session, 0);
+      $tickTuck=false;
+      $('#clock').css('color','white');
+      $('#big').text('Session');
+    });
+
+    $('.reset').on('click',function(){
+      clearInterval($intervalId);
+      clockFace(25,0);
+      $minute=25;
+      $tickTuck=false;
+    });
+
+    $('.pause').on('click',function(){
+      clearInterval($intervalId);
+    });
+
+  });
+
+  function clockFace (minute, second){
+     if (second<10){ second='0' + second}
+     else {second=second }
+     time=minute + ':' + second;
+     $("#clock").text(time);
+  };
+
+  function startClock (){
+      if ($totalTime==0 && $('#big').text()=='Session'){
+        $('#big').text('Break');
+        $totalTime=$breaks *60;
+        clockFace($breaks, 0);
+        $minute=$breaks;
+      }
+       else if ($totalTime==0 && $('#big').text()=='Break') {
+         $('#big').text('Session');
+         $totalTime=$Session *60;
+         clockFace($Session, 0);
+         $minute=$Session;
+       }
+      else { timePass();
+        if ( $totalTime >10){
+          $('#clock').css('color','green');
+        }
+        if ($totalTime <=10){
+          $('#clock').css('color','red');
+        }
+       }
+  }
+
+  function timePass(){
+    $totalTime=($minute *60)+$second;
+    $totalTime=$totalTime-1;
+    $minute=parseInt($totalTime /60);
+    $second=$totalTime %60;
+    clockFace($minute, $second);
+    return $totalTime;
+  }
 
 });
-
-function clockFace (hour, minute){
-   if (minute<10){ minute='0' + minute}
-   else {minute=minute }
-   time=hour + ':' + minute;
-   $("#clock").text(time);
-};
